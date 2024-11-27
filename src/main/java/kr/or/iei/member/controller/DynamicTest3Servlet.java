b21aef77220d42ea870c3dc9c2a527458288318a
김찬희 <blackeagle10@icloud.com>
2024-11-27 18:04:39 +0900

    Revert "everthing is functioning correctly"
    This reverts commit 6072e0a8339b590ae8afd6e46f1b30d65e5536d2.

--- src/main/java/kr/or/iei/member/controller/DynamicTest3Servlet.java
+++ src/main/java/kr/or/iei/member/controller/DynamicTest3Servlet.java
@@ -46,8 +46,6 @@ public class DynamicTest3Servlet extends HttpServlet {
 		for (int i = 0; i < list.size(); i++) {
 			System.out.println(list.get(i).toString());
 		}
-
-		request.getRequestDispatcher("/views/dynamicQueryTest.jsp").forward(request, response);
 	}
 
 	/**
